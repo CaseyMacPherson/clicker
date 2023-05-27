55badcb841d19d3e3b037900c4eb0e779edb5e90
Casey MacPherson <casey@macphersonclan.org>
2023-05-27 14:15:57 -0700

    does the clicking thing based on the first process that matches the name

--- MouseOperations.cs
+++ MouseOperations.cs
@@ -27,7 +27,7 @@ public class MouseOperations
 
     [DllImport("user32.dll")]
     private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
-
+    
     public static void SetCursorPosition(int x, int y) 
     {
         SetCursorPos(x, y);
--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System.Diagnostics;
 using clicker;
 
-Console.WriteLine("Hello, World!");
-MouseOperations.SetCursorPosition(0,0);
+var application = Process.GetProcessesByName("RealmGrinderDesktop").FirstOrDefault();
+
+if (application != null)
+{
+    UIWindowsOperations.SetForegroundWindow(application.MainWindowHandle);
+    UIWindowsOperations.GetWindowRect(application.MainWindowHandle, out var windowDimensions);
+    
+    var horizontalCenter = windowDimensions.Left + ((windowDimensions.Right - windowDimensions.Left) / 2);
+    var verticalCenter = windowDimensions.Bottom - ((windowDimensions.Bottom - windowDimensions.Top) / 2);
+    
+    MouseOperations.SetCursorPosition(horizontalCenter,verticalCenter);
+
+    for (var i = 0; i < 10000; i++)
+    {
+        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+        Thread.Sleep(10);
+        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+        Thread.Sleep(10);
+    }
+
+}
+
